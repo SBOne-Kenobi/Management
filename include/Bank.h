db2fefaa8300a51001d8e2ec79e9c6ae37f1dc75
SBOne-Kenobi <snob-1432e@mail.ru>
2020-03-30 00:22:34 +0500

    realise Bank add structs Offer, Demand add requests processing

--- include/Bank.h
+++ include/Bank.h
@@ -2,33 +2,58 @@
 #define MANAGEMENT_BANK_H
 
 #include <vector>
+#include <random>
+#include <algorithm>
 
 namespace Management {
 
+  struct DemandOffer {
+    double UMat; //ед. сырья на игрока.
+    int MinPrice; //минимальная цена покупки сырья.
+    double UProd; //ед. продукции на игрока.
+    int MaxPrice; //максимальная цела продажи продукции.
+  };
+
+  struct Demand {
+    int UMat;
+    int Price;
+    int Priority;
+
+    bool operator<(const Demand &x) const;
+  };
+
+  struct Offer {
+    int UProd;
+    int Price;
+    int Priority;
+
+    bool operator<(const Offer &x) const;
+  };
+
   class Bank {
-  public:
-    struct DemandOffer {
-      int UMat; //ед. сырья на игрока.
-      int MinPrice; //минимальная цена покупки сырья.
-      int UProd; //ед. продукции на игрока.
-      int MaxPrice; //максимальная цела продажи продукции.
-    };
-
   private:
-    const std::vector<std::vector<double>> probOfPriceChange; //вероятности изменения уровня цен.
-    const std::vector<DemandOffer> priceLevelInfo; //инофрмация по уровням цен.
+    std::vector<std::discrete_distribution<>> probOfPriceChange; //вероятности изменения уровня цен.
+    std::vector<DemandOffer> priceLevelInfo; //инофрмация по уровням цен.
     int price_level; //текущий уровень цен.
+    int materials; //остаток лимита материалов.
+    int product; //остаток лимита продукции.
+
+    std::mt19937 gen; //для рандома
 
   public:
-    Bank();
+    explicit Bank(int N); //принимает общее кол-во игроков
 
-    ~Bank();
+    ~Bank() = default;
 
-    int getPriceLevel() const;
+    [[nodiscard]] int getPriceLevel() const;
 
-    void setNewPriceLevel();
+    void setNewPriceLevel(int N);
 
-    DemandOffer getInfo() const;
+    [[nodiscard]] DemandOffer getInfo() const;
+
+    std::vector<int> requestOfMat(const std::vector<Demand> &demand); //возвращает индексы принятых запросов.
+
+    std::vector<int> requestOfProd(const std::vector<Offer> &offer); //возвращает индексы принятыз запросов.
 
     friend class Management;
   };
--- src/common/Bank.cpp
+++ src/common/Bank.cpp
@@ -1,1 +1,78 @@
 #include "Bank.h"
+
+namespace Management {
+  Bank::Bank(int N) : price_level(2), gen(std::random_device().operator()()) {
+    probOfPriceChange = {
+            {1. / 3,  1. / 3,  1. / 6, 1. / 12, 1. / 12},
+            {1. / 4,  1. / 3,  1. / 4, 1. / 12, 1. / 12},
+            {1. / 12, 1. / 4,  1. / 3, 1. / 4,  1. / 12},
+            {1. / 12, 1. / 12, 1. / 4, 1. / 3,  1. / 4},
+            {1. / 12, 1. / 12, 1. / 6, 1. / 3,  1. / 3}
+    };
+    priceLevelInfo = {
+            {1.0, 800, 3.0, 6500},
+            {1.5, 650, 2.5, 6000},
+            {2.0, 500, 2.0, 5500},
+            {2.5, 400, 1.5, 5000},
+            {3.0, 300, 1.0, 4500}
+    };
+    materials = floor(N * priceLevelInfo[price_level].UMat);
+    product = floor(N * priceLevelInfo[price_level].UProd);
+  }
+
+  int Bank::getPriceLevel() const {
+    return price_level + 1;
+  }
+
+  void Bank::setNewPriceLevel(int N) {
+    price_level = probOfPriceChange[price_level](gen);
+    materials = floor(N * priceLevelInfo[price_level].UMat);
+    product = floor(N * priceLevelInfo[price_level].UProd);
+  }
+
+  DemandOffer Bank::getInfo() const {
+    return priceLevelInfo[price_level];
+  }
+
+  std::vector<int> Bank::requestOfMat(const std::vector<Demand> &demand) {
+    std::vector<int> accepted;
+    std::vector<int> ord(demand.size());
+    for (int i = 0; i < (int) demand.size(); i++)
+      ord[i] = i;
+    std::sort(ord.begin(), ord.end(), [&demand](int a, int b) { return demand[a] < demand[b]; });
+    for (auto i : ord) {
+      if (demand[i].Price < getInfo().MinPrice)
+        break;
+      if (demand[i].UMat > materials)
+        continue;
+      materials -= demand[i].UMat;
+      accepted.push_back(i);
+    }
+    return accepted;
+  }
+
+  std::vector<int> Bank::requestOfProd(const std::vector<Offer> &offer) {
+    std::vector<int> accepted;
+    std::vector<int> ord(offer.size());
+    for (int i = 0; i < (int) offer.size(); i++)
+      ord[i] = i;
+    std::sort(ord.begin(), ord.end(), [&offer](int a, int b) { return offer[a] < offer[b]; });
+    for (auto i : ord) {
+      if (offer[i].Price > getInfo().MaxPrice)
+        break;
+      if (offer[i].UProd > product)
+        continue;
+      product -= offer[i].UProd;
+      accepted.push_back(i);
+    }
+    return accepted;
+  }
+
+  bool Demand::operator<(const Demand &x) const {
+    return Price > x.Price || (Price == x.Price && Priority < x.Priority);
+  }
+
+  bool Offer::operator<(const Offer &x) const {
+    return Price < x.Price || (Price == x.Price && Priority < x.Priority);
+  }
+}
