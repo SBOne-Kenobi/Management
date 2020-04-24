0325ba077b75c3ea1abd35b8f15e7840f724f27d
SBOne-Kenobi <snob-1432e@mail.ru>
2020-04-24 19:44:08 +0500

    + AdaptMode in Cell

--- Assets/Scripts/View/PlayerCanvas/Cell.cs
+++ Assets/Scripts/View/PlayerCanvas/Cell.cs
@@ -8,6 +8,10 @@ public class Cell : MonoBehaviour
     public Fabric fabric = null;
     PlayerCanvas parent;
 
+    public GameObject Production;
+    public GameObject Upgrade;
+    public GameObject Buy;
+
     private void Start()
     {
         parent = GetComponentInParent<PlayerCanvas>();
@@ -18,17 +22,33 @@ public class Cell : MonoBehaviour
         switch (parent.Mode)
         {
             case ProductionMode.PROD:
+                Production.SetActive(true);
+                Upgrade.SetActive(false);
+                Buy.SetActive(false);
                 break;
             case ProductionMode.BUY:
+                Production.SetActive(false);
+                Upgrade.SetActive(false);
+                Buy.SetActive(true);
                 break;
             case ProductionMode.UPGRADE:
+                Production.SetActive(false);
+                Upgrade.SetActive(true);
+                Buy.SetActive(false);
                 break;
         }
+    }
+
+    private void AdaptFabric()
+    {
+        
     }
 
     private void Update()
     {
         AdaptMode();
+        if (Production.activeSelf)
+            AdaptFabric();
     }
 
 }
