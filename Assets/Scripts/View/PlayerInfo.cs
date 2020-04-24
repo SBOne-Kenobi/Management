5802521e7b90e3ee7f7ea934b03249741da68e94
Elizaveta Artser <eartser19@gmail.con>
2020-04-24 18:36:08 +0500

    fixed toolbar

--- Assets/Scripts/View/PlayerInfo.cs
+++ Assets/Scripts/View/PlayerInfo.cs
@@ -14,11 +14,11 @@ public class PlayerInfo : MonoBehaviour
     virtual protected void Awake()
     {
         if (Prod == null)
-            Prod = transform.Find("Prod/ProdVal");
+            Prod = transform.Find("Prod");
         if (Mat == null)
-            Mat = transform.Find("Mat/MatVal");
+            Mat = transform.Find("Mat");
         if (Mon == null)
-            Mon = transform.Find("Mon/MonVal");
+            Mon = transform.Find("Mon");
         if (PlayerName == null)
             PlayerName = transform.Find("PlayerName");
     }
@@ -26,11 +26,11 @@ public class PlayerInfo : MonoBehaviour
     virtual protected void Update()
     {
         if (Prod != null)
-            Prod.GetComponent<Text>().text = player.Director.Product.ToString();
+            Prod.GetComponent<Text>().text = "P: " + player.Director.Product.ToString();
         if (Mat != null)
-            Mat.GetComponent<Text>().text = player.Director.Materials.ToString();
+            Mat.GetComponent<Text>().text = "M: " + player.Director.Materials.ToString();
         if (Mon != null)
-            Mon.GetComponent<Text>().text = player.Director.Money.ToString();
+            Mon.GetComponent<Text>().text = "$: " + player.Director.Money.ToString();
         if (PlayerName != null)
             PlayerName.GetComponent<Text>().text = player.Name;
     }
