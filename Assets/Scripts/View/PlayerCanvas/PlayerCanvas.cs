b084727f189d038a860eed30256814b210ef329e
SBOne-Kenobi <snob-1432e@mail.ru>
2020-04-24 14:24:59 +0500

    + ProdElems

---
+++ Assets/Scripts/View/PlayerCanvas/PlayerCanvas.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCanvas : MonoBehaviour
+{
+    public Player player;
+    private Switcher switcher;
+    public bool IsOwner => switcher.GetPlayer() == player;
+
+    private void Awake()
+    {
+        switcher = FindObjectOfType<Switcher>();
+    }
+}
---
+++ Assets/Scripts/View/PlayerCanvas/ProdElement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProdElement : MonoBehaviour
+{
+    Player player;
+    PlayerCanvas parent;
+
+    private void Start()
+    {
+        var c = GetComponentsInParent<Canvas>();
+        parent = c[c.Length - 1].GetComponent<PlayerCanvas>();
+        player = parent.player;
+        enabled = parent.IsOwner;
+    }
+}
